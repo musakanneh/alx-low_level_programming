361d546b26c814ae42c4635d375d3a9791b14f06
musakanneh <mkanneh17@alustudent.com>
2020-09-16 17:25:36 +0200

    0x00. C - Hello, World Added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,8 @@
 * Return: Always 0 (Success)
 */
 
-int main(void){
+int main(void)
+{
     printf("Size of a char: %ld byte(s)\n", sizeof(char));
     printf("Size of an int: %ld byte(s)\n", sizeof(int));
     printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
