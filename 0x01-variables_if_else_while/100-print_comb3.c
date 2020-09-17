ee47e9da57dd9959dc234c226da7a4bb667feeb4
musakanneh <mkanneh17@alustudent.com>
2020-09-17 20:51:55 +0200

    Added advanced text #11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints a list of double didgits
+ *
+ * Description: digits should not be repeated twice
+ * Return: Always(0) Success
+*/
+
+int main (void)
+{
+    int i, j;
+    
+    for (i = 48; i < 58; i++)
+    {
+        for (j = 48; j < 58; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            putchar(i);
+            putchar(j);
+
+            if (i == 56 && j == 57)
+            {
+                break;
+            }
+
+            else
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
+}
+
