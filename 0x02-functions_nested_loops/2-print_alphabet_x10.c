149fb3653af8b1bf97c9d9d43eb5fadd940221ff
musakanneh <mkanneh17@alustudent.com>
2020-09-22 17:04:28 +0200

    Added 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
-/*
- * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+/**
+ * print_alphabet - prints all aphabets in lowercase
  *
- * return (0)
+ * Return: Always(0) Success
  */
 
 void print_alphabet_x10(void)
