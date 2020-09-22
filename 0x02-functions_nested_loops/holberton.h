b4c041aa47c7371f43f80448c561b65cba9022d1
musakanneh <mkanneh17@alustudent.com>
2020-09-22 14:56:06 +0200

    Added 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 int _putchar(char);
 
 void print_alphabet(void);
@@ -24,5 +21,3 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
-
-#endif
