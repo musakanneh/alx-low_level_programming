1e00b3ca3d3e10c05659207157568a1c1daf935d
musakanneh <mkanneh17@alustudent.com>
2020-09-22 14:59:55 +0200

    Added 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	char c[9] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(holberton[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
