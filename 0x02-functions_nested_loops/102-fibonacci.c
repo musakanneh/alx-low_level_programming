04b9337f8030fdb5bf5e129ce37b28e70e8123fc
musakanneh <mkanneh17@alustudent.com>
2020-09-22 21:59:29 +0200

    Added advanced task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints first 50 numbers of the fibonacci sequence
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, n;
+	int first_count, sec_count, next_num;
+
+	first_count = 1;
+	sec_count = 2;
+	n = 50;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 1)
+		{
+			printf("%d", first_count);
+		}
+		else
+		{
+			printf("%d, ", first_count);
+			next_num = first_count + sec_count;
+			first_count = sec_count;
+			sec_count = next_num;
+		}
+	}
+	printf("\n");
+	return (0);
+}
