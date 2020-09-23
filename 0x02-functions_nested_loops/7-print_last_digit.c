8cc80988dcb8eea69aca75694d45b30c00904fc3
musakanneh <mkanneh17@alustudent.com>
2020-09-23 19:08:12 +0200

    Quick fix added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,16 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	last_digit = n % 10;
 
 	if (last_dight < 0)
 	{
 		last_digit *= -1;
 	}
 
-	_putchar(last_digit + 0);
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
