62372fa235347e0c1af07e3d0e9eb45481b28ca2
musakanneh <mkanneh17@alustudent.com>
2020-09-23 20:54:22 +0200

    Added advanced tasks

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonnaci numbers
+ *
+ * Description: prints from 1 to 98
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+	int num1, num2, i, n;
+	int next_num;
+
+	n = 98;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i == 98)
+		{
+			printf("%d, ", num2);
+		}
+		else
+		{
+			printf("%d, ", num2);
+			next_fib = num1 + num2;
+			num1 = num2;
+			num2 = next_num;
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
