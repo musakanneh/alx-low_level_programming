123979c8a69fe765f1fba7ba6e8ae3de5e6483fc
musakanneh <mkanneh17@alustudent.com>
2020-09-24 16:07:42 +0200

    Added 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,6 @@
  * Return: Always (0)
  */
 
-int main(void)
-{
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
-	return (0);
-}
-
 void print_triangle(int size)
 {
 	int i, j;
