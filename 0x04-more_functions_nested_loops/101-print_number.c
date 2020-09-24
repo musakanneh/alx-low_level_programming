d49af03b6abdb79f118ad265499f3d4dfcd0409d
musakanneh <mkanneh17@alustudent.com>
2020-09-24 21:42:39 +0200

    Added advanced task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * print_number - print number
+ *
+ * Return: Always (0)
+ */
+
+void print_number(int n)
+{
+	if ((n < 0) && (n >= -9))
+	{
+		_putchar('-');
+		_putchar((n * -1) + '0');
+	}
+	else if (n <= -10)
+	{
+		_putchar('-');
+		_putchar(((n / 10) * -1) + '0');
+		_putchar(((n % 10)  * -1) + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	else if ((n > 0) && (n <= 9))
+	{
+		_putchar(n + '0');
+	}
+	else if ((n >= 10) && (n <= 99))
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n >= 100) && (n <= 999))
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n >= 1000) && (n <= 9999))
+	{
+		_putchar((n / 1000) + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+}
