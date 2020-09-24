e9d6bb71fc374ab3a212dc34f031b1beed30375a
musakanneh <mkanneh17@alustudent.com>
2020-09-24 21:15:27 +0200

    Attempted on adv task 1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints out the highest prime
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int n, i, d;
+
+	d = 50829601;
+	n = 150;
+
+	for (i = 3; i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			 n = n / i;
+		}
+	}
+	printf("%d", d);
+	printf("\n");
+	return (0);
+}
