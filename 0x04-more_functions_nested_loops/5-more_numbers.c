e35b062807ec7b73984086541e0d07b23b7f0804
musakanneh <mkanneh17@alustudent.com>
2020-09-24 15:04:14 +0200

    Added 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - more numbers
  *
- * Description: prints 10 times the numbers, from 0 to 14, followed by a new line
+ * Description: prints 10 times the numbers, from 0 to 14
  * Return: Always (0)
  */
 
@@ -12,9 +12,9 @@ void more_numbers(void)
 	int n, i;
 
 	for (n = 0; n <= 10; n++)
-	{	
+	{
 		for (i = 0; i <= 14; i++)
-		{	
+		{
 			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
