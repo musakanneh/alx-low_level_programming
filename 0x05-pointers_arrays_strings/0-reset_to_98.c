4097ff4778e7e3732c9509a893e97f24958d9f3e
musakanneh <mkanneh17@alustudent.com>
2020-09-28 02:06:38 +0200

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#indlude "holberton.h"
+
+/**
+ * reset_to_98 - reset to 98
+ * @n: input value
+ *
+ * Description: takes a pointer to an int as parameter and updates the value
+ * Return: Always (0)
+ */
+
+void reset_to_98(int *n)
+{
+	
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# 0x05. C - Pointers, arrays and strings
+
