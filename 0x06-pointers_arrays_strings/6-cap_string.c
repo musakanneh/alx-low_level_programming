82241271ca6f503b07d65a9326d4ce945680b00b
musakanneh <mkanneh17@alustudent.com>
2020-10-01 18:58:10 +0200

    soln: 4, 5, 5 added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,27 @@
  * Return: always (0)
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int i;
+	i = 0;
 
+	while(s[i] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';'
+				|| s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' 
+				|| s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
