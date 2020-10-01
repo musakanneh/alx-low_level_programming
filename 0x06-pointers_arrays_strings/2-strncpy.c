6be56f2b6fe64346360d9c723026841234bb1554
musakanneh <mkanneh17@alustudent.com>
2020-10-01 14:08:13 +0200

    Soln: 2,3 added

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,14 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n)
-	{
-		++i;
-	}
-	for (i = 0; i < n && src[i] != '\0'; ++i)
+	for ( ; i < n && src[i] != '\0'; ++i)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		++i;
+	}
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,27 @@
  * @s2: second input char
  *
  * Description: the function compares two strings
- * Return: always (0)
+ * Return: character count
  */
 
 int _strcmp(char *s1, char *s2)
 {
+	int i, j, diff;
 
+	for (i = 0; s1[i] != '\0'; ++i)
+	{
+		for (j = 0; s2[j] != '\0'; ++j)
+		{
+			if (s1[i] == s2[j])
+			{
+				continue;
+			}
+			else
+			{
+				diff = s1[i] - s2[j];
+				return (diff);
+			}
+		}
+	}
+	return (0);
 }
