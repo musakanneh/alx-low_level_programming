7caaf706c630b0bbdcb96fcab5eb26ae0e528971
musakanneh <mkanneh17@alustudent.com>
2020-10-01 20:52:09 +0200

    Removed #7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "holberton.h"     
+
+/**
+ * leet - encode string
+ * @str: input string
+ *
+ * Description: encodes a string into 1337
+ * Return: always (0
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	i = 0;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
+
+	for ( ; str[i] != '\0'; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			if (s[j] == str[i])
+			{
+				str[i] = s1[j];
+			}
+		}
+	}
+	return (str);
+}
