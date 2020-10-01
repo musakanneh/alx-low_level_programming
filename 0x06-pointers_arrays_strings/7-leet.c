62b137846dc32cfcd1c6e056f712bbb6f5ff8ada
musakanneh <mkanneh17@alustudent.com>
2020-10-01 19:21:17 +0200

    soln: #7 added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,12 +2,29 @@
 
 /**
  * leet - encode string
- *
+ * @str: input string
  * Description: encodes a string into 1337
  * Return: always (0)
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
+	int i, j;
 
+	i = 0;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (s[i] == s[j])
+			{
+				s[i] = s1[j];
+			}
+		}
+		i++;
+	}
+	return (str);
 }
