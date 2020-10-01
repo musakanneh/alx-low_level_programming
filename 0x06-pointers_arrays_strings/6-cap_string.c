f35871746606aa72615023fc6ec2cd90570cc484
musakanneh <mkanneh17@alustudent.com>
2020-10-01 19:03:18 +0200

    Betty error fixed: #6wq

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - capitalize string
- *
+ * @s: input string
  * Description: capitalizes all words of a string
  * Return: always (0)
  */
@@ -10,17 +10,17 @@
 char *cap_string(char *s)
 {
 	int i;
-	i = 0;
 
-	while(s[i] != '\0')
+	i = 0;
+	while (s[i] != '\0')
 	{
 		if (s[0] >= 'a' && s[0] <= 'z')
 		{
 			s[0] = s[0] - 32;
 		}
-		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';'
-				|| s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' 
-				|| s[i] == '}')
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '!' || s[i] == '?' || s[i] == '('
+				|| s[i] == ')' || s[i] == '{' || s[i] == '}')
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
