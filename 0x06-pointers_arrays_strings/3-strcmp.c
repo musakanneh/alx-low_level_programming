6fbc84679550b40ed8ccb765456005013d94874f
musakanneh <mkanneh17@alustudent.com>
2020-10-01 14:34:22 +0200

    Soln: 2,3 added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, diff;
+	int i, diff;
 
 	for (i = 0; s1[i] != '\0'; ++i)
 	{
-		for (j = 0; s2[j] != '\0'; ++j)
+		if (s1[i] < s2[i])
 		{
-			if (s1[i] == s2[j])
-			{
-				continue;
-			}
-			else
-			{
-				diff = s1[i] - s2[j];
-				return (diff);
-			}
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
+		else
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
 		}
 	}
 	return (0);
