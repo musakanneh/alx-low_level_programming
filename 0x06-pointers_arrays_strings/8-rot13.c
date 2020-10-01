88d9e7aaa571bf78d368f503118d36a3096d5535
musakanneh <mkanneh17@alustudent.com>
2020-10-01 20:59:07 +0200

    Removed #7

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,7 +7,21 @@
  * Return: always (0)
  */
 
-char *rot13(char *)
+char *rot13(char *str)
 {
+	int i = 0, k;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	for (; str[i] != '\0'; i++)
+	{
+		for (k = 0; k <= 51; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+	}
+	return (str);
 }
