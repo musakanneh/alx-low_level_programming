c4cda756ef9c9104b2dada41180dc0d8be9d48dd
musakanneh <mkanneh17@alustudent.com>
2020-10-01 15:10:00 +0200

    Soln: 4 added

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,5 +11,17 @@
 
 void reverse_array(int *a, int n)
 {
+	int left, right,  tmp;
+	
+	left = 0;
+	right = n - 1;
 
+	while (left < right)
+	{
+		tmp = a[left];
+		a[left] = a[right];
+		a[right] = tmp;
+		left++;
+		right--;
+	}
 }
