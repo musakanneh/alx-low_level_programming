8bd6fcc651b4460c4b50ac397241107ffc5be650
musakanneh <mkanneh17@alustudent.com>
2020-10-05 20:34:16 +0200

    Task 7 added

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,15 +1,27 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * set_string - set value
- * @s: input string
- * @to: input character
+ * @a: input string
+ * @size: input character
  *
  * Description: sets the value of a pointer to a char
  * Return: always 0
  */
 
-void set_string(char **s, char *to)
+void print_diagsums(int *a, int size)
 {
+	int i, j, sum = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		sum += *(a + i * size + i);
+	}
+	printf("%i, ", sum);
+	for (j = 0; j < size; j++)
+	{
+		sum += *(a + size * (j + 1) - (j + 1));
+	}
+	printf("%i\n", sum);
 }
