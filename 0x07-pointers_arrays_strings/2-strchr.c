396e68a4afc8b0d648053a9d0db2134c8813d6e6
musakanneh <mkanneh17@alustudent.com>
2020-10-05 14:50:05 +0200

    Task #2 added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,8 @@
  * @c: input character
  *
  * Description: locates a character in a string
- * Return: returns a ptr to the 1st occurrence of the char c in str s, else, return NULL
+ * Return: returns a ptr to the 1st occurrence of
+ * the char c in str s, else, return NULL
  */
 
 char *_strchr(char *s, char c)
