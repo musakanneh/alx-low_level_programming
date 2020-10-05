4ab5df4079d57a371537b4e582373f9ca9e0da86
musakanneh <mkanneh17@alustudent.com>
2020-10-05 18:57:40 +0200

    Task #3 added

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-
-	while (s[i] == 
+	unsigned int i, j;
+	
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (!accept[j])
+		{
+			break;
+		}
+	}
+	return (i);
 }
