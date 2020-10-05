3e431478ca59ed51b141d60f24d243094c446af6
musakanneh <mkanneh17@alustudent.com>
2020-10-05 19:15:15 +0200

    Task 4 added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - search a string
@@ -11,5 +12,34 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j, len, exist = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	len = i;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				if (j <= len)
+				{
+					len = j;
+					exist = 1;
+				}
+			}
+		}
+	}
+	if (exist == 1)
+	{
+		return (&s[len]);
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (0);
 }
