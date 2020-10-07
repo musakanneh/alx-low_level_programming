d520f9857a4ebde22938dd07effeaed56710ab8d
musakanneh <mkanneh17@alustudent.com>
2020-10-07 22:04:55 +0200

    Added No. 7

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,40 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - returns prime num
- * @n: input integer value
- *
- * Description: returns 1 if the input integer is a 
- * prime number, otherwise return 0
- * Return: returns the state of the inout value
+ * is_prime - find a prime number
+ * @i: input int i
+ * @j: input int j
+ * Return: boolean val of the state of inputs
  */
+
+int is_prime(int i, int j)
+{
+	if (j == 1)
+	{
+		return (1);
+	}
+	if (i % j == 0)
+	{
+		return (0);
+	}
+	return (is_prime(i, j - 1));
+}
+
+ /**
+  * is_prime_number - printing prime numbers
+  * @n: input integer value
+  *
+  * Description: returns 1 if the input integer is a
+  * prime number, otherwise return 0
+  * Return: returns the state of the inout value
+  */
 
 int is_prime_number(int n)
 {
-
+	if (n < 3)
+	{
+		return (0);
+	}
+	return (is_prime(n, n - 1));
 }
+
