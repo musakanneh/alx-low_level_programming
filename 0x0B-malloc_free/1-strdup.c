9913915f15ab811d3caacbfa03f0d1ed5e57a5cf
musakanneh <mkanneh17@alustudent.com>
2020-10-13 17:18:45 +0200

    0x0B. C - malloc, free added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _strdup - allocates space in memory
@@ -10,5 +12,20 @@
 
 char *_strdup(char *str)
 {
+	char *duplicate_num;
+	int i, len = 0;
+
+	duplicate_num = malloc(sizeof(char) * (len + 1));
+	if (str == 0 || duplicate_num == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		duplicate_num[i] = str[i];
+		len++;
+	}
+	duplicate_num[len] = '\0';
+	return (duplicate_num);
 
 }
