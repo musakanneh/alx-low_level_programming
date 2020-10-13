947e7c86981b18fb4b31795c1b957ef0795e1d21
musakanneh <mkanneh17@alustudent.com>
2020-10-13 19:58:06 +0200

    0x0B. C - malloc, free added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * alloc_grid - pointer to a 2 dimensional array of integers
@@ -10,5 +12,26 @@
 
 int **alloc_grid(int width, int height)
 {
+	int **arr;
+	int i;
 
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+	arr = (int **) malloc(height * sizeof(int *));
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+	}
+	if (arr != NULL)
+	{
+		return (arr);
+	}
+	return (NULL);
 }
