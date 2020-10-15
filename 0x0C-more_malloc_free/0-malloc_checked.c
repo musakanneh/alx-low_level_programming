fd5efd29a4584b2c48d6b2b48b1bb3b001d06c74
musakanneh <mkanneh17@alustudent.com>
2020-10-15 13:01:30 +0200

    More malloc, free added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: input character
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+	
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	return (ptr);
+}
