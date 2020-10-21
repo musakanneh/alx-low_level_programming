dda896b10b3d27aefe7f2e418351dded027c08ec
musakanneh <mkanneh17@alustudent.com>
2020-10-21 14:58:07 +0200

    0x0F. C - Function pointers added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
+#ifndef _function_pointers_
+#define _function_pointers_
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
+#endif
