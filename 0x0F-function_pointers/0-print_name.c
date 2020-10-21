5bf11869675cdfb2009bd147d6a224c8b5f372b1
musakanneh <mkanneh17@alustudent.com>
2020-10-21 14:50:27 +0200

    0x0F. C - Function pointers added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: input params for name
+ * @f: reps a function pointer
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
