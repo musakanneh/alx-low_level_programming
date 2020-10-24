56b7335b163b34a68d2f28104c66cbdcb92b1536
musakanneh <mkanneh17@alustudent.com>
2020-10-24 23:56:44 +0200

    Task 2 sonl updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef variadic_functions_
 #define variadic_functions_
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
