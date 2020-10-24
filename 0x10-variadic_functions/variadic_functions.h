3f3e9be2cec882c2c6a5d235616baa78a18e753a
musakanneh <mkanneh17@alustudent.com>
2020-10-24 13:41:02 +0200

    Task 0 done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#ifndef variadic_functions_
+#define variadic_functions_
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
