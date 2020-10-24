173f6b50271b0cfac652a81e5f54d5f8021ffcbe
musakanneh <mkanneh17@alustudent.com>
2020-10-24 13:26:03 +0200

    Task 0 done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sum_them_all - sum given inputs
+ * @n: input params
+ * Return: sum of nums
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list lst;
+	unsigned int sum = 0, i;
+
+	va_start(lst, n);
+	for (i = 0; i < n; i++)
+	{
+		int n = va_arg(lst, unsigned int);
+
+		sum += n;
+	}
+	va_end(lst);
+	return (sum);
+}
+
+
