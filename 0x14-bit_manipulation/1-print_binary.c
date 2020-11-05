d9deffdc32f2bcdeacdec61ce4518d3ee6cdd376
musakanneh <mkanneh17@alustudent.com>
2020-11-05 23:03:28 +0200

    Bit manipulation added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,53 +1,15 @@
 #include "holberton.h"
 
 /**
- * _to_power - powers a number base to
- * @base: base input
- * @power: power input
- * Return: the power of two the
- */
-
-unsigned long int _to_power(int base, int power)
-{
-	unsigned long int res = 1;
-
-	while (power)
-	{
-		res *= base;
-		power--;
-	}
-	return (res);
-}
-
-/**
  * print_binary - prints the binary representation of a number
- * @n: input integer
+ * @n: the number to be converted
+ *
+ * Return: Void
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int test;
-
-	test = _to_power(2, sizeof(unsigned long int) * 8 - 1);
-	int start = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (test)
-	{
-		if (!(test & n) && start)
-		{
-			_putchar('0');
-		}
-		else if (test & n)
-		{
-			_putchar('1');
-			start = 1;
-		}
-		test = test >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
-
