b0f99b1534655c81ca529b7b4801b960d6896c8f
musakanneh <mkanneh17@alustudent.com>
2020-12-13 11:07:14 +0200

    Free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,11 +2,18 @@
 
 /**
  * free_dlistint -  a function that free a dlistint_t list
- *
+ * @head: head of the linkedlist
  * Return: Nothing!
  */
-
-
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head;
+		head = head->next;
+		free(temp_node);
+	}
+	head = NULL;
 }
