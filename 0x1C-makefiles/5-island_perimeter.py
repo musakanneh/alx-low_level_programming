80705a1b667fc41b99e4401a5e6cae7d10bd3921
musakanneh <mkanneh17@alustudent.com>
2021-02-19 21:00:15 +0200

    MakeFiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+
+    Args:
+        grid(matrix): input grid
+
+    Description:
+        Traverse the land mass, and
+        for the lake's nearest neighbor
+        then return the total length
+
+    """
+    y = 0
+    x = 0
+    for i in range(1, len(grid) - 1:
+            for col in range(1, len(grid[row]) - 1):
+                if grid[row][col] == 1:
+                    if grid[row][col - 1] == 0:
+                        y += 1
+                    if grid[row][col + 1] == 0:
+                        y += 1
+                    if grid[row - 1][col] == 0:
+                        x += 1
+                    if grid[row + 1][col] == 0:
+                        x += 1
+    return x + y
